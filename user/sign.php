4905ef0b9305bc13348688e8d526179374787a3b
Fisherfu <113712591+Fisherfu@users.noreply.github.com>
2025-06-22 12:57:14 +0800

    Add JSON header for AJAX response

--- user/sign.php
+++ user/sign.php
@@ -1,4 +1,8 @@
 <?php
+header('Content-Type: application/json');
+require_once("../config.php");
+
+<?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 header('Content-Type: application/json');

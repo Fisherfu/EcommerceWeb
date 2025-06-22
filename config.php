b16a29455feab13cf726c6cd72bf3721d60136e9
Fisherfu <113712591+Fisherfu@users.noreply.github.com>
2025-06-22 14:00:17 +0800

    Add full DB error debug output

--- config.php
+++ config.php
@@ -3,9 +3,20 @@ $DB_HOST = 'sql12.freesqldatabase.com';
 $DB_USER = 'sql12786152';
 $DB_PASS = 'lg5nIVxRrT';
 $DB_NAME = 'sql12786152';
+$DB_PORT = 3306; // Optional but explicit
 
-$link = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+// 建立連線並加上錯誤訊息
+$link = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
+
 if (!$link) {
-    die('DB 連線失敗: ' . mysqli_connect_error());
+    // 印出詳細的錯誤資訊
+    die("DB 連線失敗：<br>" .
+        "Host: {$DB_HOST}<br>" .
+        "User: {$DB_USER}<br>" .
+        "DB: {$DB_NAME}<br>" .
+        "Error: " . mysqli_connect_error());
 }
 ?>
+
+
+
